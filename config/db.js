55edd5641c88d21f4359f3bec3dde41d8042bcf3
nidha <nidhanidhu1106@gmail.com>
2025-03-11 21:21:26 +0530

    partially completed

--- config/db.js
+++ config/db.js
@@ -1,16 +1,19 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
+const products= require('../models/productSchema')
 
 
 const connectDB = async() =>{
     try{
-       await mongoose.connect(process.env.MONGODB_URI);
+       await mongoose.connect(process.env.MONGODB_URI,{});
        console.log('database connected');
+       console.log(await products.find({}))
     }catch (error)
     {
       console.log('database connection error',error.message);
       process.exit(1);
     }
 }
+
 
 module.exports = connectDB;
