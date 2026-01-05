182ca88dbe5d46a3efe92f5dd361e3b1ea70393e
nidha <nidhanidhu1106@gmail.com>
2026-01-05 18:12:42 +0530

    fix:nodemailer

--- controllers/user/profileController.js
+++ controllers/user/profileController.js
@@ -22,8 +22,8 @@ const sendVerificationEmail = async (email, otp) => {
     try {
         const transporter = nodemailer.createTransport({
             host: "smtp.gmail.com",
-            port: 587,
-            secure: false,
+            port: 465,
+            secure: true,
             auth: {
                 user: process.env.NODEMAILER_EMAIL,
                 pass: process.env.NODEMAILER_PASSWORD,
